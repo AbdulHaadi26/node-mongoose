876201d0d73e4328390c7cd59bb8324c48866ff2
Abdul Haadi <abdulhaadirao@gmail.com>
2021-10-03 00:52:04 +0500

    Installed mongoose

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const http = require('http');
 const compression = require('compression');
-const { sequelize } = require('./sequelize/db');
 
 //Middleware functions
 app.use(express.json({ limit: '100mb' }));
@@ -16,13 +15,6 @@ app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors());
 app.use(compression({ filter: shouldCompress }));
 
-sequelize.sync({ force: true });
-
-app.use('/belongsTo', require('./belongsTo'));
-app.use('/hasMany', require('./hasMany'));
-app.use('/hasOne', require('./hasOne'));
-app.use('/belongsToMany', require('./belongsToMany'));
-app.use('/aggregate', require('./aggregate'));
 
 function shouldCompress(req, res) {
     if (req.headers["x-no-compression"]) return false;
